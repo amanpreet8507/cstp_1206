1178180a15c1dfffbb63dd530f978a911097c1c3
amanpreet8507 <105711554+amanpreet8507@users.noreply.github.com>
2023-02-19 13:21:31 -0800

    second commit_midtermPart2

--- Week 7/script.js
+++ Week 7/script.js
@@ -1,21 +1,54 @@
-//const APIKEY = "https://www.themoviedb.org/1b977ff48db9f4e31e40912f11c4ea6b";
-//https://api.themoviedb.org/3/movie/550?api_key=1b977ff48db9f4e31e40912f11c4ea6b
+const APIKEY = "1b977ff48db9f4e31e40912f11c4ea6b";
+
+//"https://www.themoviedb.org/1b977ff48db9f4e31e40912f11c4ea6b";
+//https://api.themoviedb.org/3/search/movie?${APIKEY}&query=man
+
 
 async function getMovieInfo() {
-    let data = await fetch('https://www.themoviedb.org');
+    let data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=war`);
     let dataWithJSON = await data.json();
-    let finalOutputArray = dataWithJSON.data; //.data check it in consloe 
+    let finalOutputArray = dataWithJSON.results; //.data check it in consloe 
+    //console.log(finalOutputArray);
+    createUI(finalOutputArray);
+
 }
 
-let container = document.querySelector('#maincontainer'); // cotainer is like the whole page containing all the movie infor
+async function getGenre(){
+    let genre = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKEY}`);
+    let genWithJSON = await genre.json();
+    let genres = genWithJSON.genres;
+    return genres;
+   
+    
+}
 
-for(let i = 0; i < finalOutputArray.length; i++){
+async function matchGenre(array){
+    let genWithJSON = await getGenre();
+    let movieGenre = [];
+    
+    for(let j = 0; j < array.length; j++){
+        for(let i = 0; i < genWithJSON.length; i++){
+            if(genWithJSON[i].id === array[j]){
+                movieGenre += (`${genWithJSON[i].name} `);
+            }
+        }
+    }
+    //console.log(movieGenre);
+    return movieGenre;
+}
+
+
+//let container = document.querySelector('#maincontainer'); // cotainer is like the whole page containing all the movie infor
+
+async function createUI(finalArray){
+    let container = document.querySelector('#movieInfo-section'); // cotainer is like the whole page containing all the movie infor    
+for(let i = 0; i < finalArray.length; i++){
     let movieElement = document.createElement("div");
     movieElement.classList.add("movieInfo");  // movieElement is div for each seprate movie conatinning info and movieInfo is the class for that
 
 //IMAGE APPENDING
     let img = document.createElement("img"); // another img tag created
-    //img.src = finalOutputArray[i].POSTER;    //whatever name in the console array
+    img.src = `https://image.tmdb.org/t/p/original/${finalArray[i].poster_path}`;    //whatever name in the console array
     img.classList.add("moviePoster");    // made a class for img tag to use in css
     
     movieElement.appendChild(img);  // appended the img to the movieElement div so now each div is going to have the movie image
@@ -23,15 +56,15 @@ for(let i = 0; i < finalOutputArray.length; i++){
 
 //TITLE AND RELEASED YEAR APPENDING  
     let titleAndReleasedYear = document.createElement("p");
-    //titleAndReleasedYear.textContent = `${finalOutputArray[i].title_of_the_movie} ${finalOutputArray[i].released_year}`;
+    titleAndReleasedYear.textContent = `${finalArray[i].title} ${finalArray[i].release_date}`;
     titleAndReleasedYear.classList.add("titleAndYear");
 
-    movieElement.appendChild(movieTitleAndReleasedYear);
+    movieElement.appendChild(titleAndReleasedYear);
     
 
 // DESCRIPTION APPENDING
     let description = document.createElement("p");
-    //description.textContent = finalOutputArray[i].description;
+    description.textContent = finalArray[i].overview;
     description.classList.add("des");
 
     movieElement.appendChild(description);
@@ -40,7 +73,7 @@ for(let i = 0; i < finalOutputArray.length; i++){
 
 //GENRE APPENDING
     let genre = document.createElement("p");
-    //description.textContent = finalOutputArray[i].genre;
+    genre.textContent = await matchGenre(finalArray[i].genre_ids);
     genre.classList.add("gen");
 
     movieElement.appendChild(genre);
@@ -48,8 +81,8 @@ for(let i = 0; i < finalOutputArray.length; i++){
 
 //EACH MOVIE ELEMENT IN CONTAINER
     container.appendChild(movieElement); // appended the each movieElement to the cotainer(whole page)
-    console.log(finalOutputArray[i]); // just to see the content from api
+    //console.log(finalArray[i]); // just to see the content from api
 
+    }
 }
-
 getMovieInfo();
